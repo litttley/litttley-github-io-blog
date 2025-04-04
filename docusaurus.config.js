964b558ac322f7ee9dad0674e01cf97ea70ed59a
617914118@qq.com <617914118@qq.com>
2025-04-04 14:20:08 +0800

    优化

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://litttley.github.io/litttley-github-io-blog/',
+  url: 'https://litttley.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/litttley-github-io-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
